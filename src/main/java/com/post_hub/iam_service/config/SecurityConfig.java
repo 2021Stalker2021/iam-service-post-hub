40bfd545c2bf43209c195ea3858bf433f9fd04e2
2021Stalker2021 <edik.korben@gmail.com>
2025-11-11 05:08:07 +0300

    update SecurityConfig

--- src/main/java/com/post_hub/iam_service/config/SecurityConfig.java
+++ src/main/java/com/post_hub/iam_service/config/SecurityConfig.java
@@ -1,17 +1,39 @@
 package com.post_hub.iam_service.config;
 
+import com.post_hub.iam_service.security.filter.JwtRequestFilter;
+import com.post_hub.iam_service.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 @Configuration // Делает класс конфигурационным
 @EnableWebSecurity // Включает поддержку Spring Security
+@EnableMethodSecurity // управление доступом на уровне методов
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+
+    private static final String POST = "POST";
+
+    private static final PathPatternRequestMatcher[] NOT_SECURED_URLS = new PathPatternRequestMatcher[] {
+            PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.POST, "/auth/login"),
+            PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.POST, "/auth/register"),
+    };
 
     @Bean
     // Метод для настройки правил безопасности
@@ -20,14 +42,32 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Зачем отключать: REST API не использует cookies/sessions
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                ); // Доступ к всем endpoint'ам разрешён
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(NOT_SECURED_URLS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptions -> exceptions.
+                        authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // каждый новый пароль будет хэшироватся перед сохранением в базу
+        return new BCryptPasswordEncoder(); // каждый новый пароль будет хэшироваться перед сохранением в базу
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserService userService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder()); // сравнивает зашифрованные пароли
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
