587021ebd9cadf225608c65cbc64f679514a4e43
2021Stalker2021 <edik.korben@gmail.com>
2026-02-11 21:54:31 +0300

    add Test getCommentById

--- src/test/java/com/post_hub/iam_service/service/CommentServiceTest.java
+++ src/test/java/com/post_hub/iam_service/service/CommentServiceTest.java
@@ -5,17 +5,24 @@ import com.post_hub.iam_service.model.dto.comment.CommentDTO;
 import com.post_hub.iam_service.model.entity.Comment;
 import com.post_hub.iam_service.model.entity.Post;
 import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.model.exception.NotFoundException;
 import com.post_hub.iam_service.repository.CommentRepository;
 import com.post_hub.iam_service.repository.PostRepository;
 import com.post_hub.iam_service.repository.UserRepository;
 import com.post_hub.iam_service.service.impl.CommentServiceImpl;
 import com.post_hub.iam_service.utils.ApiUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
@@ -64,5 +71,32 @@ public class CommentServiceTest {
         testCommentDTO.setId(1);
         testCommentDTO.setMessage("TestComment");
 
+    }
+
+    @Test
+    void getCommentById_CommentExists_ReturnsCommentDTO() {
+        when(commentRepository.findByIdAndDeletedFalse(1)).thenReturn(Optional.of(testComment));
+        when(commentMapper.toDto(testComment)).thenReturn(testCommentDTO);
+
+        CommentDTO result = commentService.getCommentById(1).getPayload();
+
+        assertNotNull(result);
+        assertEquals(testCommentDTO.getId(), result.getId());
+        assertEquals(testCommentDTO.getMessage(), result.getMessage());
+
+        verify(commentRepository, times(1)).findByIdAndDeletedFalse(1);
+        verify(commentMapper, times(1)).toDto(any(Comment.class));
+    }
+
+    @Test
+    void getCommentById_CommentNotExists_ThrowsException() {
+        when(commentRepository.findByIdAndDeletedFalse(999)).thenReturn(Optional.empty());
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> commentService.getCommentById(999));
+
+        assertTrue(exception.getMessage().contains("not found"));
+
+        verify(commentRepository, times(1)).findByIdAndDeletedFalse(999);
+        verify(commentMapper, never()).toDto(any(Comment.class));
     }
 }
