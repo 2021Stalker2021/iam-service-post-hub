dd5fc2419e6b057514326ca25333029daf749803
2021Stalker2021 <edik.korben@gmail.com>
2026-02-12 20:04:47 +0300

    add Test registerUser

--- src/main/java/com/post_hub/iam_service/model/request/user/RegistrationUserRequest.java
+++ src/main/java/com/post_hub/iam_service/model/request/user/RegistrationUserRequest.java
@@ -4,11 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationUserRequest implements Serializable {
 
     @NotBlank
